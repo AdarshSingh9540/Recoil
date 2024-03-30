7b0cc7410f513845c7249471fe9f9b219de056a3
Adarsh <adarsh9540984202@gmail.com>
2024-03-31 01:31:28 +0530

    add it

--- src/App.js
+++ src/App.js
@@ -3,44 +3,54 @@ import { RecoilRoot } from 'recoil';
 import './App.css';
 import { useRecoilState, useRecoilValue } from 'recoil';
 import { countAtom } from './store/atom/count';
+import { networkAtom } from './atoms';
 
 function App() {
+  const networkNotiCount = useRecoilValue(networkAtom);
   return (
     <RecoilRoot>
       <>
-        <CountRender />
-        <Buttons />
+        {/* <CountRender />
+        <Buttons /> */}
+
+        <button>Home</button>
+        <button>My Network ({networkNotiCount >=100 ? "99+" :networkNotiCount})</button>
+        <button>Jobs ()</button>
+        <button>Messaging</button>
+        <button>Notification</button>
+
+        <button>Me</button>
       </>
     </RecoilRoot>
   );
 }
 
-function CountRender() {
-  const count = useRecoilValue(countAtom);
-  return (
-    <div>
-      <b>{count}</b>
-    </div>
-  );
-}
+// function CountRender() {
+//   const count = useRecoilValue(countAtom);
+//   return (
+//     <div>
+//       <b>{count}</b>
+//     </div>
+//   );
+// }
 
-function Buttons() {
-  const [count, setCount] = useRecoilState(countAtom);
+// function Buttons() {
+//   const [count, setCount] = useRecoilState(countAtom);
 
-  const increaseCount = () => {
-    setCount(count + 1);
-  };
+//   const increaseCount = () => {
+//     setCount(count + 1);
+//   };
 
-  const decreaseCount = () => {
-    setCount(count - 1);
-  };
+//   const decreaseCount = () => {
+//     setCount(count - 1);
+//   };
 
-  return (
-    <div>
-      <button onClick={increaseCount}>Increase</button>
-      <button onClick={decreaseCount}>Decrease</button>
-    </div>
-  );
-}
+//   return (
+//     <div>
+//       <button onClick={increaseCount}>Increase</button>
+//       <button onClick={decreaseCount}>Decrease</button>
+//     </div>
+//   );
+// }
 
 export default App;
